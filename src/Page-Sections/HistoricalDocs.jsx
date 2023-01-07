83a25681f27d08f82d21e3321367243e5869fa57
Daniel Rook <110010478+Dev-Rook@users.noreply.github.com>
2023-01-06 19:52:57 -0500

    Historical Document Links

--- src/Page-Sections/HistoricalDocs.jsx
+++ src/Page-Sections/HistoricalDocs.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 import Styles from "../Styles/Page-Section-Styles/HistoryDocs.module.scss";
 
 import HistoryData from "../Assests/Data/HistoryPdf.json";
