18b9d759388066fb50c5864ef66add325ca3ea68
Daniel Rook <110010478+Dev-Rook@users.noreply.github.com>
2023-01-08 00:01:38 -0500

    Calander

--- src/Pages/Sailing.jsx
+++ src/Pages/Sailing.jsx
@@ -33,11 +33,7 @@ const Sailing = () => {
       </div>
 
       <div className={Styles.Poster_Container}>
-        <img
-          src={`https://github.com/Dev-Rook/yacht-club/blob/master/src/Assets/Images/J22%20Skedule.jpg?raw=true`}
-          alt=""
-          className={Styles.Fishing_Poster}
-        />
+      <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FJamaica&showTitle=0&src=ZGV2LnJvb2sxMjFAZ21haWwuY29t&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uam0jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%2333B679&color=%230B8043"  width="100%" height="600" frameborder="0" scrolling="no"></iframe>
       </div>
 
       <J22Sailing />
