749f740737b3f51ec6f11b766940d8691ad71aa0
Daniel Rook <110010478+Dev-Rook@users.noreply.github.com>
2023-01-18 07:57:44 -0500

    Commit

--- src/Pages/Sailing.jsx
+++ src/Pages/Sailing.jsx
@@ -13,13 +13,13 @@ const Sailing = () => {
     <div className={Styles.Page}>
       <PageHead Title={"Do You Sail?"} />
 
-      <div className={Styles.Poster_Container}>
+      {/* <div className={Styles.Poster_Container}>
         <iframe
           src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FJamaica&showTitle=0&src=ZGV2LnJvb2sxMjFAZ21haWwuY29t&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uam0jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%2333B679&color=%230B8043"
           width="100%"
           height="600"
         ></iframe>
-      </div>
+      </div> */}
 
       <J22Sailing />
       <PinappleCup />
